1e8638c3e5cf05c607f590a21d9b1ea8a88dc9e8
Alex <alexfilsecker@gmail.com>
2024-10-31 18:09:16 +1100

    cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,8 +25,6 @@ SECRET_KEY = "django-insecure-zhwf1=r04=%j!41)6@_c#*67ycuggvk#_vmkkiv--pk9c^qoby
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -39,17 +37,21 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "api",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
 
 ROOT_URLCONF = "backend.urls"
 
